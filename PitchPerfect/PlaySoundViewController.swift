7a729b49162b05219ab562b3aa7c4e025238abb5
Dinu Kumarasiri <sandarumk@gmail.com>
2016-06-10 22:15:59 +0530

    Added Nested Stack Views with buttons

--- PitchPerfect/PlaySoundViewController.swift
+++ PitchPerfect/PlaySoundViewController.swift
@@ -12,6 +12,14 @@ class PlaySoundViewController: UIViewController {
     
     var recordedAudioURL:NSURL!
 
+    @IBOutlet weak var dathvaderButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
